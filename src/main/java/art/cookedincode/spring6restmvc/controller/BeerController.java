257c78b917298d51a9e804d3d024a53abb294404
Georgi Ivanov <georgi.ivanov85@hotmail.com>
2024-06-24 16:00:03 +0200

    add controller

---
+++ src/main/java/art/cookedincode/spring6restmvc/controller/BeerController.java
@@ -0,0 +1,22 @@
+package art.cookedincode.spring6restmvc.controller;
+
+import art.cookedincode.spring6restmvc.model.Beer;
+import art.cookedincode.spring6restmvc.service.BeerService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+import java.util.UUID;
+
+@Slf4j
+@AllArgsConstructor
+@Controller
+public class BeerController {
+    private final BeerService beerService;
+
+    public Beer getBeerById(UUID id) {
+        log.debug("Get Beer by Id - in controller");
+
+        return beerService.getBeerById(id);
+    }
+}
---
+++ src/main/java/art/cookedincode/spring6restmvc/service/BeerService.java
@@ -0,0 +1,12 @@
+package art.cookedincode.spring6restmvc.service;
+
+import art.cookedincode.spring6restmvc.model.Beer;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface BeerService {
+
+    List<Beer> listBeers();
+    Beer getBeerById(UUID id);
+}
