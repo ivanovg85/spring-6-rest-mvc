6061e9b07b2c5fdb9f1ac47f5d4054984cedd7a3
Georgi Ivanov <georgi.ivanov85@hotmail.com>
2024-06-26 20:19:18 +0200

    add beer csv data

---
+++ src/main/java/art/cookedincode/spring6restmvc/service/BeerCsvService.java
@@ -0,0 +1,11 @@
+package art.cookedincode.spring6restmvc.service;
+
+import art.cookedincode.spring6restmvc.model.BeerCSVRecord;
+
+import java.io.File;
+import java.util.List;
+
+public interface BeerCsvService {
+
+    List<BeerCSVRecord> convertCSV(File csvFile);
+}
