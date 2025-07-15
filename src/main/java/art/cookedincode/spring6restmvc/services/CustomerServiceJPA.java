2f2bd3fa17e3e896f1d201edea49c32fdcf34305
Georgi Ivanov <georgi.ivanov85@hotmail.com>
2025-07-16 00:48:10 +0300

    add JPA services and integration tests

---
+++ src/main/java/art/cookedincode/spring6restmvc/services/CustomerServiceJPA.java
@@ -0,0 +1,58 @@
+package art.cookedincode.spring6restmvc.services;
+
+import art.cookedincode.spring6restmvc.mappers.CustomerMapper;
+import art.cookedincode.spring6restmvc.model.CustomerDTO;
+import art.cookedincode.spring6restmvc.repositories.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Georgi Ivanov
+ */
+@Service
+@Primary
+@RequiredArgsConstructor
+public class CustomerServiceJPA implements CustomerService {
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+
+    @Override
+    public Optional<CustomerDTO> getCustomerById(UUID id) {
+        return Optional.ofNullable(customerMapper.customerToCustomerDto(customerRepository.findById(id)
+                .orElse(null)));
+    }
+
+    @Override
+    public List<CustomerDTO> listCustomers() {
+        return customerRepository.findAll()
+                .stream()
+                .map(customerMapper::customerToCustomerDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public CustomerDTO saveCustomer(CustomerDTO customer) {
+        return null;
+    }
+
+    @Override
+    public void updateCustomerById(UUID customerId, CustomerDTO customer) {
+
+    }
+
+    @Override
+    public void deleteCustomerById(UUID customerId) {
+
+    }
+
+    @Override
+    public void patchCustomerById(UUID customerId, CustomerDTO customer) {
+
+    }
+}
---
+++ src/test/java/art/cookedincode/spring6restmvc/controller/CustomerControllerIT.java
@@ -0,0 +1,60 @@
+package art.cookedincode.spring6restmvc.controller;
+
+import art.cookedincode.spring6restmvc.entities.Customer;
+import art.cookedincode.spring6restmvc.model.CustomerDTO;
+import art.cookedincode.spring6restmvc.repositories.CustomerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Created by Georgi Ivanov
+ */
+@SpringBootTest
+class CustomerControllerIT {
+
+    @Autowired
+    CustomerController customerController;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void testCustomerIdNotFound() {
+        assertThrows(NotFoundException.class, () -> {
+            customerController.getCustomerById(UUID.randomUUID());
+        });
+    }
+
+    @Test
+    void testGetById() {
+        Customer customer = customerRepository.findAll().getFirst();
+
+        assertThat(customerController.getCustomerById(customer.getId())).isNotNull();
+    }
+
+    @Test
+    void testListCustomers() {
+        List<CustomerDTO> dtos = customerController.listCustomers();
+
+        assertThat(dtos.size()).isEqualTo(3);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testEmptyList() {
+        customerRepository.deleteAll();
+        List<CustomerDTO> dtos = customerController.listCustomers();
+
+        assertThat(dtos.size()).isEqualTo(0);
+    }
+}
