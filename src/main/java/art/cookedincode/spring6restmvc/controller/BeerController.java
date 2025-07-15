e3dc8e113c14b9afd90c93aa5c1fa5f31b985bba
Georgi Ivanov <georgi.ivanov85@hotmail.com>
2025-07-15 15:09:19 +0300

    add Location header to createBeer

--- src/main/java/art/cookedincode/spring6restmvc/controller/BeerController.java
+++ src/main/java/art/cookedincode/spring6restmvc/controller/BeerController.java
@@ -4,6 +4,7 @@ import art.cookedincode.spring6restmvc.model.Beer;
 import art.cookedincode.spring6restmvc.services.BeerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,7 +26,10 @@ public class BeerController {
     public ResponseEntity handlePost(@RequestBody Beer beer) {
         Beer savedBeer = beerService.saveNewBeer(beer);
 
-        return new ResponseEntity(HttpStatus.CREATED);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer/" + savedBeer.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
     @GetMapping
