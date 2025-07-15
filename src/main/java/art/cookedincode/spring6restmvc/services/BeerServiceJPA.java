8e03a0f852e58825ca2e3f414bc08eb06bcdf72f
Georgi Ivanov <georgi.ivanov85@hotmail.com>
2025-07-16 00:19:38 +0300

    bootstrap data

---
+++ src/main/java/art/cookedincode/spring6restmvc/services/BeerServiceJPA.java
@@ -0,0 +1,53 @@
+package art.cookedincode.spring6restmvc.services;
+
+import art.cookedincode.spring6restmvc.mappers.BeerMapper;
+import art.cookedincode.spring6restmvc.model.BeerDTO;
+import art.cookedincode.spring6restmvc.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Created by Georgi Ivanov
+ */
+@Service
+@Primary
+@RequiredArgsConstructor
+public class BeerServiceJPA implements BeerService {
+    private final BeerRepository beerRepository;
+    private final BeerMapper beerMapper;
+
+    @Override
+    public List<BeerDTO> listBeers() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<BeerDTO> getBeerById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public BeerDTO saveNewBeer(BeerDTO beer) {
+        return null;
+    }
+
+    @Override
+    public void updateBeerById(UUID beerId, BeerDTO beer) {
+
+    }
+
+    @Override
+    public void deleteBeerById(UUID beerId) {
+
+    }
+
+    @Override
+    public void patchBeerById(UUID beerId, BeerDTO beer) {
+
+    }
+}
