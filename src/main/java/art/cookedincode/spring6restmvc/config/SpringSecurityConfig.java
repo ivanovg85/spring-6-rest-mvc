925a8d34c9f946ab5b07d0740d545945602f4d84
Georgi Ivanov <georgi.ivanov85@hotmail.com>
2025-11-14 13:38:51 +0200

    add Atlassian RestAssured openapi test and validation

--- src/main/java/art/cookedincode/spring6restmvc/config/SpringSecurityConfig.java
+++ src/main/java/art/cookedincode/spring6restmvc/config/SpringSecurityConfig.java
@@ -2,6 +2,7 @@ package art.cookedincode.spring6restmvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -9,6 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 /**
  * Created by Georgi Ivanov
  */
+@Profile("!test")
 @Configuration
 public class SpringSecurityConfig {
 
